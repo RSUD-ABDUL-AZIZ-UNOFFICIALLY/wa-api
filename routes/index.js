6a68c5d7013a7fbbca88940e16d84d0175f0e6a6
fakhryhizballah <falehry88@gmail.com>
2026-07-30 14:20:49 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -16,5 +16,6 @@ routes.post('/wa/setUnread', middleware.check, wa.setUnread);
 routes.get('/wa/getChat', middleware.check, base.getChat);
 routes.post('/wa/getImageChat', middleware.check, wa.getImageChat);
 routes.post('/wa/contacname', middleware.check, wa.getContactName);
+routes.get('/health', (req, res) => res.status(200).json({ status: true, message: "OK" }));
 
 module.exports = routes;
